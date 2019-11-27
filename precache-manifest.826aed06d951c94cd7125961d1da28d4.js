1e73f40ff6f003bab69c1979a58df2711562d04b
Alex Sales <asales2005@gmail.com>
2019-11-26 23:06:54 -0800

    Updates

---
+++ precache-manifest.826aed06d951c94cd7125961d1da28d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e25b9d97cec9eb9c8abc096907632f",
+    "url": "/neighborhood-map-touristy-things/index.html"
+  },
+  {
+    "revision": "7d972db5f13df779a669",
+    "url": "/neighborhood-map-touristy-things/static/css/main.e7ffac1b.chunk.css"
+  },
+  {
+    "revision": "6040b54b61192e0d4d5a",
+    "url": "/neighborhood-map-touristy-things/static/js/2.94cd085a.chunk.js"
+  },
+  {
+    "revision": "7d972db5f13df779a669",
+    "url": "/neighborhood-map-touristy-things/static/js/main.44e3ee39.chunk.js"
+  },
+  {
+    "revision": "75b58fa3ce1d1271ddd6",
+    "url": "/neighborhood-map-touristy-things/static/js/runtime-main.a136d090.js"
+  },
+  {
+    "revision": "be2c63a10eb1190b6944e98bc86a91b9",
+    "url": "/neighborhood-map-touristy-things/static/media/heart-active-v2.be2c63a1.svg"
+  },
+  {
+    "revision": "0e1f1f094384abe4cb0b3e94c62f81ad",
+    "url": "/neighborhood-map-touristy-things/static/media/heart-inactive-v2.0e1f1f09.svg"
+  },
+  {
+    "revision": "0e0abb7feb19db4a0085f79a5e91eda9",
+    "url": "/neighborhood-map-touristy-things/static/media/heart-inactive-v3.0e0abb7f.svg"
+  }
+]);
