85f41b52c2cfdbf1d6a936209678deff12b021e1
Alex Sales <asales2005@gmail.com>
2019-12-06 17:34:03 -0800

    - Install Firebase; - Add close SVG image to use on Modals and Faves components;

--- neighborhood-map-touristy-things/src/components/UI/Backdrop/Backdrop.js
+++ neighborhood-map-touristy-things/src/components/UI/Backdrop/Backdrop.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Backdrop.module.css';
+import closeBtn from '../../../assets/close-24px.svg';
 
 const Backdrop = props => {
-  // const { show = false, ...otherProps } = props;
   return props.show ? (
-    <div className={classes.Backdrop} onClick={props.click}></div>
+    <div className={classes.Backdrop} onClick={props.closeModal}>
+      <NavLink to='/' className={classes.closeBtn}>
+        <img src={closeBtn} alt='close modal button' />
+      </NavLink>
+    </div>
   ) : null;
 };
 
