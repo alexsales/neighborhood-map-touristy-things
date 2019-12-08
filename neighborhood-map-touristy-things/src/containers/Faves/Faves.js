b0df647e3ebf089702ed11ab06bdfde2b8edf6e0
Alex Sales <asales2005@gmail.com>
2019-12-08 11:57:39 -0800

    - Update Fave component to display test login account; - Add functionality to delete favorited places; - When deleting a fave, update both Firebase database and Redux store;

--- neighborhood-map-touristy-things/src/containers/Faves/Faves.js
+++ neighborhood-map-touristy-things/src/containers/Faves/Faves.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Backdrop from '../../components/UI/Backdrop/Backdrop';
 import { connect } from 'react-redux';
+import { instanceFirebase } from '../../axios';
 import classes from './Faves.module.css';
 
 const Faves = props => {
   const userFavesArr = props.faves.map((fave, index) => {
     return (
-      <li key={index}>
+      <li
+        key={index}
+        onClick={() => {
+          const idToDelete = fave.placeId;
+          instanceFirebase
+            .delete(
+              '/users/' +
+                localStorage.getItem('userId') +
+                `/faves-list/${idToDelete}.json`
+            )
+            .then(resp => {
+              props.onDeleteFromFaves(idToDelete);
+            });
+        }}>
         <div>{props.faves[index].name}</div>
         <div>{props.faves[index].address}</div>
       </li>
@@ -17,7 +31,6 @@ const Faves = props => {
     <div className={classes.Faves}>
       <div className={classes.favesList}>
         <h2>Faves</h2>
-        <ul>{userFavesArr}</ul>
         {props.userToken === null ? (
           <>
             <button
@@ -29,6 +42,7 @@ const Faves = props => {
             <span>to load/save your favorites</span>
           </>
         ) : null}
+        <ul>{userFavesArr}</ul>
       </div>
       <Backdrop show={true} />
     </div>
@@ -44,7 +58,14 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => {
-  return {};
+  return {
+    onDeleteFromFaves: idToDelete => {
+      dispatch({
+        type: 'DELETEFROMFAVES',
+        payload: idToDelete
+      });
+    }
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Faves);
