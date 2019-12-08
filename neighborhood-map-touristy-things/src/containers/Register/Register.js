3aaa6ce5420fa01d5cf2fb2dcc5034b1e2b41e5d
Alex Sales <asales2005@gmail.com>
2019-12-08 11:56:17 -0800

    - Add styles to Register component; - Add register button, but keep functionality TBA;

--- neighborhood-map-touristy-things/src/containers/Register/Register.js
+++ neighborhood-map-touristy-things/src/containers/Register/Register.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import Backdrop from '../../components/UI/Backdrop/Backdrop';
+import classes from './Register.module.css';
 
 const Register = props => {
-  return <div>Register</div>;
+  return (
+    <div>
+      <div className={classes.Register}>
+        <div className={classes.notRegistered}>
+          <h2>Register</h2>
+          <p className={classes.regDisabled}>
+            <span>registration disabled</span>
+          </p>
+          <div>
+            <button
+              onClick={e => {
+                // props.history.push('/auth');
+                e.preventDefault();
+                e.stopPropagation();
+              }}>
+              REGISTER
+            </button>{' '}
+          </div>
+        </div>
+        <Backdrop show={true} />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
